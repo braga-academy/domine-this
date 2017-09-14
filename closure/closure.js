2282e7366ba26e88ae30cea334742976f4c9881e
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-09-14 20:57:20 -0300

    Closure - exercise

--- closure/closure.js
+++ closure/closure.js
@@ -19,3 +19,22 @@ console.log(luciano.checkPassword(123456));
 console.log(luciano);
 console.log(luciano.getName());
 console.log(luciano.getAge());
+
+/* -------------------------
+// Exercise
+----------------------------*/
+
+function add(a, b){
+    return a + b;
+}
+
+function binAdd(a){
+    return function(b){
+        return add(a,b);
+    }
+}
+
+const add10 = binAdd(10);
+
+console.log(add10(25));
+console.log(binAdd(25)(25));
