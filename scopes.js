69f445d59d00b2b63df72d2d77914c2d967d5ca2
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-09-14 16:13:25 -0300

    Scopes

---
+++ scopes.js
@@ -0,0 +1,15 @@
+var CONFIG = {
+    host: "https://google.com"
+};
+
+var name = "Teste";
+
+function init() {
+    var name = "Luciano";
+    console.log(CONFIG, name);
+};
+
+
+
+console.log(name);
+init();
