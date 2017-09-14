35b37f1ec3a2b086868a58d2e01a6bc3d129c726
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-09-14 17:49:36 -0300

    construct

---
+++ constructs/construct.js
@@ -0,0 +1,21 @@
+function Pessoa(name, age) {
+    this.name = name;
+    this.age = age || 1;
+
+    this.speak =  function(words){
+        console.log(this.name + ' says: ' + words)
+    };
+    this.olden =  function(years){
+        this.age += years || 1;
+        console.log(this.age)
+    }
+}
+
+const luciano = new Pessoa("Luciano", 25)
+const bia = new Pessoa("Bia", 2)
+
+console.log(luciano);
+console.log(bia);
+
+luciano.speak('Hello Word!');
+luciano.olden(5);
